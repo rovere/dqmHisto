6218895c60ad89d1feebea60f0e848a9c1ee9d72
Antanas Norkus <antanas.norkus@cern.ch>
2015-09-25 14:46:57 +0200

    Added better checkinf of min release in sequence making.

--- checkNewCMSSW.py
+++ checkNewCMSSW.py
@@ -15,7 +15,7 @@ os.chdir("/home/DQMHisto")
 reports_dir = os.listdir("report");
 reports_to_do = []
 if len(reports_dir) == 0:
-   min_release = 'CMSSW_6_2_0_pre7'
+   min_release = 'CMSSW_7_4_0_pre5'
 else:
    min_release = min(reports_dir)
 
@@ -31,11 +31,12 @@ for elem in reports_to_do:
     if elem in reports_dir:
         print "%s report already exists" %(elem)
     else:
-        #if elem.find('SLHC') != -1:
+        if elem.find('SLHC') != -1:
+           continue ##we ignore SLHC releases for now
         if ((elem > min_release and elem != 'CMSSW_7_0_0_XROOTD')):
         #if elem == 'CMSSW_7_2_0_pre2': #test purposes only
             print "Report to do: %s" %(elem)
-            os.system("./dqmHisto/test.sh "+ elem+"")
+            #os.system("./dqmHisto/test.sh "+ elem+"")
             print "Done!"
             break  #we work only on single release per day
 
@@ -49,12 +50,14 @@ for elem in reports_dir:
         existing_releases.append(tmp[1])
 existing_releases = list(set(existing_releases))
 
+#default min release
 reports_to_do = []
 if len(existing_releases) == 0:
-   min_release = 'CMSSW_6_2_0_pre7'
+   min_release = 'CMSSW_7_4_0_pre5'
 else:
    min_release = min(existing_releases)
 
+print "#DEBUG# min release: %s" % (min_release)
 #xml_data = xml.dom.minidom.parseString(os.popen("curl -s --insecure 'https://cmstags.cern.ch/tc/ReleasesXML/?anytype=1").read())
 for arch in xml_data.documentElement.getElementsByTagName("architecture"):
     scram_arch = arch.getAttribute('name')
@@ -66,18 +69,28 @@ for elem in reports_to_do:
         #print "SEQUENCES > %s report already exists" %(elem[0])
         continue
     else:
+        slhc_rel_list = [el for el in existing_releases if el.find('slhc') != -1]
+        patch_rel_list = [el for el in existing_releases if el.find('patch') != -1]
         if elem[0].find('SLHC') != -1:
+            continue
             if (elem[0] > "CMSSW_6_2_0_SLHC7" and elem[0] != 'CMSSW_7_0_0_XROOTD'):
             #if elem == 'CMSSW_7_1_0_pre3': #test purposes only
-                print "SEQUENCES > Report to do: %s arch %s" %(elem[0], elem[1])
-                os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " " + "auto:upgrade2019")
+                print "SEQUENCES > SLHC report to do: %s arch %s" %(elem[0], elem[1])
+                #os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " " + "auto:upgrade2019") ##for now we dont need SLHC
                # print "SEQUENCES > Done!"
-               # break
+                #break
+        elif elem[0].find('patch') != -1:
+            if ((elem[0] > min(patch_rel_list) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
+                print "SEQUENCES > Patch report to do: %s arch %s" %(elem[0], elem[1])
+                print "./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + "auto:mc"
+                os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " "+ "auto:mc")
+                break ## we also want single sequence per day
         else:
-            if ((elem[0] > min_release and elem[0] != 'CMSSW_7_0_0_XROOTD')):
+            other_releases = set(existing_releases) - set(patch_rel_list) - set(slhc_rel_list)
+            if ((elem[0] > min(other_releases) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
             #if elem[0] == 'CMSSW_7_2_0_pre4': #test purposes only
                 print "SEQUENCES > Report to do: %s arch %s" %(elem[0], elem[1])
                 print "./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + "auto:mc"
                 os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " "+ "auto:mc")
-                #print "SEQUENCES > Done!"
-                #break
+                print "SEQUENCES > Done!"
+                break ## we also want single sequence per day
