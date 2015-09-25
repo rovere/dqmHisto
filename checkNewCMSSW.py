1d1dbece3e490b54b7283f08e766e5b0063a43fd
Antanas Norkus <antanas.norkus@cern.ch>
2015-09-25 15:46:32 +0200

    Some code clean-up

--- checkNewCMSSW.py
+++ checkNewCMSSW.py
@@ -64,33 +64,44 @@ for arch in xml_data.documentElement.getElementsByTagName("architecture"):
     for project in arch.getElementsByTagName("project"):
         release = str(project.getAttribute('label'))
         reports_to_do.append((release, scram_arch))
+
+slhc_rel_list = [el for el in existing_releases if el.find('slhc') != -1]
+patch_rel_list = [el for el in existing_releases if el.find('patch') != -1]
 for elem in reports_to_do:
     if elem[0] in existing_releases:
         #print "SEQUENCES > %s report already exists" %(elem[0])
         continue
     else:
-        slhc_rel_list = [el for el in existing_releases if el.find('slhc') != -1]
-        patch_rel_list = [el for el in existing_releases if el.find('patch') != -1]
         if elem[0].find('SLHC') != -1:
             continue
             if (elem[0] > "CMSSW_6_2_0_SLHC7" and elem[0] != 'CMSSW_7_0_0_XROOTD'):
             #if elem == 'CMSSW_7_1_0_pre3': #test purposes only
-                print "SEQUENCES > SLHC report to do: %s arch %s" %(elem[0], elem[1])
+                print "SEQUENCES > SLHC report to do: %s arch %s" % (elem[0], elem[1])
                 #os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " " + "auto:upgrade2019") ##for now we dont need SLHC
                # print "SEQUENCES > Done!"
                 #break
         elif elem[0].find('patch') != -1:
             if ((elem[0] > min(patch_rel_list) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
-                print "SEQUENCES > Patch report to do: %s arch %s" %(elem[0], elem[1])
-                print "./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + "auto:mc"
-                os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " "+ "auto:mc")
+                print "SEQUENCES > Patch report to do: %s arch %s" % (elem[0], elem[1])
+                print "./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
+                        elem[1] + "auto:mc"
+
+                os.system("./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
+                        elem[1] + " "+ "auto:mc")
+
                 break ## we also want single sequence per day
         else:
-            other_releases = set(existing_releases) - set(patch_rel_list) - set(slhc_rel_list)
+            other_releases = set(existing_releases) - set(patch_rel_list) -\
+                set(slhc_rel_list)
+
             if ((elem[0] > min(other_releases) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
             #if elem[0] == 'CMSSW_7_2_0_pre4': #test purposes only
-                print "SEQUENCES > Report to do: %s arch %s" %(elem[0], elem[1])
-                print "./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + "auto:mc"
-                os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " "+ "auto:mc")
+                print "SEQUENCES > Report to do: %s arch %s" % (elem[0], elem[1])
+                print "./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
+                        elem[1] + "auto:mc"
+
+                os.system("./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
+                        elem[1] + " "+ "auto:mc")
+
                 print "SEQUENCES > Done!"
-                break ## we also want single sequence per day
+                #break ## we also want single sequence per day
