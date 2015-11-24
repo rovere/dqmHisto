4395a0513694eff4491905b48e7014b6433d2f15
Antanas Norkus <antanas.norkus@cern.ch>
2015-11-24 11:46:16 +0100

    Added a more natural alphanumeric comparison to check if release is worth to be checked

--- checkNewCMSSW.py
+++ checkNewCMSSW.py
@@ -7,8 +7,19 @@ It is being run by acrontab every midnight
 
 
 import os
+import re
 import xml.dom.minidom
+
 from time import gmtime, strftime
+
+def nat_cmp(a, b):
+    """
+    compares two alphanumeric string in "natural" way retruns 1 if a > b else -1
+    """
+
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return cmp(alphanum_key(a), alphanum_key(b))
 
 print "I started: %s" % (strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 os.chdir("/home/DQMHisto")
@@ -18,6 +29,8 @@ if len(reports_dir) == 0:
    min_release = 'CMSSW_7_4_0_pre5'
 else:
    min_release = min(reports_dir)
+patch_rel_list = [el for el in reports_dir if el.find('patch') != -1]
+print "##DEBUG Min patch release: ", min(patch_rel_list)
 
 xml_data = xml.dom.minidom.parseString(os.popen("curl -s --insecure 'https://cmssdt.cern.ch/SDT/cgi-bin/ReleasesXML/?anytype=1'").read())
 for arch in xml_data.documentElement.getElementsByTagName("architecture"):
@@ -33,10 +46,16 @@ for elem in reports_to_do:
     else:
         if elem.find('SLHC') != -1:
            continue ##we ignore SLHC releases for now
-        if ((elem > min_release and elem != 'CMSSW_7_0_0_XROOTD')):
+        elif elem.find('patch') != -1:
+            if nat_cmp(elem, min(patch_rel_list)) == 1:
+                print "Report to do: %s patch min_rel %s" %(elem, min(patch_rel_list))
+                os.system("./dqmHisto/test.sh "+ elem+"")
+                print "Done!"
+                break  #we work only on single release per day
+        elif nat_cmp(elem, min_release) == 1:
         #if elem == 'CMSSW_7_2_0_pre2': #test purposes only
             print "Report to do: %s" %(elem)
-            #os.system("./dqmHisto/test.sh "+ elem+"")
+            os.system("./dqmHisto/test.sh "+ elem+"")
             print "Done!"
             break  #we work only on single release per day
 
@@ -58,7 +77,6 @@ else:
    min_release = min(existing_releases)
 
 print "#DEBUG# min release: %s" % (min_release)
-#xml_data = xml.dom.minidom.parseString(os.popen("curl -s --insecure 'https://cmstags.cern.ch/tc/ReleasesXML/?anytype=1").read())
 for arch in xml_data.documentElement.getElementsByTagName("architecture"):
     scram_arch = arch.getAttribute('name')
     for project in arch.getElementsByTagName("project"):
@@ -69,19 +87,12 @@ slhc_rel_list = [el for el in existing_releases if el.find('slhc') != -1]
 patch_rel_list = [el for el in existing_releases if el.find('patch') != -1]
 for elem in reports_to_do:
     if elem[0] in existing_releases:
-        #print "SEQUENCES > %s report already exists" %(elem[0])
         continue
     else:
         if elem[0].find('SLHC') != -1:
             continue
-            if (elem[0] > "CMSSW_6_2_0_SLHC7" and elem[0] != 'CMSSW_7_0_0_XROOTD'):
-            #if elem == 'CMSSW_7_1_0_pre3': #test purposes only
-                print "SEQUENCES > SLHC report to do: %s arch %s" % (elem[0], elem[1])
-                #os.system("./dqmHisto/createHTMLSequences.sh "+elem[0]+" "+ elem[1] + " " + "auto:upgrade2019") ##for now we dont need SLHC
-               # print "SEQUENCES > Done!"
-                #break
         elif elem[0].find('patch') != -1:
-            if ((elem[0] > min(patch_rel_list) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
+            if nat_cmp(elem[0], min(patch_rel_list)) == 1:
                 print "SEQUENCES > Patch report to do: %s arch %s" % (elem[0], elem[1])
                 print "./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
                         elem[1] + "auto:mc"
@@ -94,7 +105,7 @@ for elem in reports_to_do:
             other_releases = set(existing_releases) - set(patch_rel_list) -\
                 set(slhc_rel_list)
 
-            if ((elem[0] > min(other_releases) and elem[0] != 'CMSSW_7_0_0_XROOTD')):
+            if nat_cmp(elem[0], min(other_releases)) == 1:
             #if elem[0] == 'CMSSW_7_2_0_pre4': #test purposes only
                 print "SEQUENCES > Report to do: %s arch %s" % (elem[0], elem[1])
                 print "./dqmHisto/createHTMLSequences.sh " + elem[0] + " " +\
@@ -104,4 +115,4 @@ for elem in reports_to_do:
                         elem[1] + " "+ "auto:mc")
 
                 print "SEQUENCES > Done!"
-                #break ## we also want single sequence per day
+                break ## we also want single sequence per day
